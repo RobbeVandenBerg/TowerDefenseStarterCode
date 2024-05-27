9189df0735f8cda110c8bebf6ad91559cfabfe74
RobbeVandenBerg <144011017+RobbeVandenBerg@users.noreply.github.com>
2024-05-27 11:54:58 +0200

    commit 1

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -7,15 +7,28 @@ public class Enemy : MonoBehaviour
     public float speed = 1f;
     public float health = 10f;
     public int points = 1;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
+    public Path path { get; set; }
+    public GameObject target { get; set; }
+
+    private int pathIndex = 1;
     void Update()
     {
-        
+        float step = speed * Time.deltaTime;
+        transform.position = Vector2.MoveTowards(transform.position, target.transform.position, step);
+
+        // check how close we are to the target 
+        if (Vector2.Distance(transform.position, target.transform.position) < 0.1f)
+        {
+            // if close, request a new waypoint 
+            target = EnemySpawner.Instance.RequestTarget(path, pathIndex);
+            pathIndex++;
+            // if target is null, we have reached the end of the path. 
+            // Destroy the enemy at this point 
+            if (target == null)
+            {
+                Destroy(gameObject);
+            }
+        }
     }
 }
--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -1,18 +1,51 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 
 public class EnemySpawner : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public static EnemySpawner Instance;
 
-    // Update is called once per frame
-    void Update()
+    public List<GameObject> Path1 = new List<GameObject>();
+    public List<GameObject> Path2 = new List<GameObject>();
+    public List<GameObject> enemies = new List<GameObject>();
+
+    private void SpawnEnemy(int type, Path path)
     {
-        
+        var newEnemy = Instantiate(enemies[type], Path1[0].transform.position, Path1[0].transform.rotation);
+        var script = newEnemy.GetComponentInParent<Enemy>();
+        script.path = path;
+        script.target = Path1[1];
+    }
+    public GameObject RequestTarget(Path path, int index)
+    {
+        List<GameObject> currentPath = null;
+        switch (path)
+        {
+            case Path.Path1:
+                currentPath = Path1;
+                break;
+            case Path.Path2:
+                currentPath = Path2;
+                break;
+            default:
+                Debug.LogError("invalid path");
+                break;
+        }
+        if (currentPath == null || index < 0 || index >= currentPath.Count)
+        {
+            Debug.LogError("invalid index or path");
+            return null;
+        }
+        else { return currentPath[index]; }
+    }
+    private void SpawnTester()
+    {
+        SpawnEnemy(0, Path.Path1);
+    }
+    private void Start()
+    {
+        InvokeRepeating("SpawnTester", 1f, 1f);
     }
 }
